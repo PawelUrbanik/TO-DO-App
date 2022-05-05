6cdbd778ee2f54dfb597dbbef7321003a6d9d976
Pawel <pawelurbanik94@gmail.com>
2022-05-05 21:03:41 +0200

    Group DTO

---
+++ src/main/java/pl/pawel/model/projection/GroupReadModel.java
@@ -0,0 +1,51 @@
+package pl.pawel.model.projection;
+
+import pl.pawel.model.Task;
+import pl.pawel.model.TaskGroups;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GroupReadModel {
+
+    private String description;
+    /**
+     * The deadline of latest task
+     */
+    private LocalDateTime deadline;
+    private Set<GroupTaskReadModel> tasks;
+
+    public GroupReadModel(TaskGroups source) {
+        this.description = source.getDescription();
+        source.getTasks().stream()
+                .map(Task::getDeadline)
+                .max(LocalDateTime::compareTo)
+                .ifPresent(lastDeadline -> deadline=lastDeadline);
+        tasks = source.getTasks().stream().map(GroupTaskReadModel::new).collect(Collectors.toSet());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public Set<GroupTaskReadModel> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<GroupTaskReadModel> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ src/main/java/pl/pawel/model/projection/GroupWriteModel.java
@@ -0,0 +1,40 @@
+package pl.pawel.model.projection;
+
+import pl.pawel.model.Task;
+import pl.pawel.model.TaskGroups;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class GroupWriteModel {
+
+    private String description;
+    private Set<GroupTaskWriteModel> tasks;
+
+    public TaskGroups toGroup(){
+        var group = new TaskGroups();
+        group.setDescription(this.description);
+        group.setTasks(
+                tasks.stream()
+                        .map(GroupTaskWriteModel::toTask)
+                        .collect(Collectors.toSet())
+        );
+        return group;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<GroupTaskWriteModel> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<GroupTaskWriteModel> tasks) {
+        this.tasks = tasks;
+    }
+}
