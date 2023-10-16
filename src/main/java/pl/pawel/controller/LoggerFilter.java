c03f6973ba5c95689e1c6af3466711f12570afee
Pawel <pawelurbanik94@gmail.com>
2023-10-16 19:29:06 +0200

    Added LoggerFilter

---
+++ src/main/java/pl/pawel/controller/LoggerFilter.java
@@ -0,0 +1,35 @@
+package pl.pawel.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LoggerFilter implements Filter {
+
+    private static Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (servletRequest instanceof HttpServletRequest) {
+            var httpReq = (HttpServletRequest) servletRequest;
+            logger.info("msg: [doFilter]" +  httpReq.getMethod() + " " + httpReq.getRequestURI());
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+}
