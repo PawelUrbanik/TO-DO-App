90e6777dd6d5557c517ea69e47b176c41be8ba64
Pawel <pawelurbanik94@gmail.com>
2023-12-02 11:58:55 +0100

    Added Exception handler for Task Group

--- src/main/java/pl/pawel/controller/TaskGroupController.java
+++ src/main/java/pl/pawel/controller/TaskGroupController.java
@@ -55,4 +55,14 @@ public class TaskGroupController {
         return ResponseEntity.noContent().build();
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
 }
