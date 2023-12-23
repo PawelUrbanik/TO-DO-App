82bfb6bb7b0c6ce192c7596b7893df986a9c5568
Pawel <pawelurbanik94@gmail.com>
2023-12-23 18:01:15 +0100

    Added own annotation for IllegalExceptionProcessing

--- src/main/java/pl/pawel/controller/TaskGroupController.java
+++ src/main/java/pl/pawel/controller/TaskGroupController.java
@@ -25,6 +25,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/groups")
+@IllegalExceptionProcessing
 public class TaskGroupController {
 
 
@@ -105,16 +106,6 @@ public class TaskGroupController {
     public ResponseEntity<?> toggleGroup(@PathVariable Long id) {
         taskGroupService.toggleGroup(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ModelAttribute("groups")
